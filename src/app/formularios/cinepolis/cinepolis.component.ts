96e701dc7b4f9e23e9aff78d16dafbfebb0234b5
DiegoArturoGuzman <diegoarturo170@gmail.com>
2025-09-25 20:40:39 -0600

    Cinepolis

---
+++ src/app/formularios/cinepolis/cinepolis.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cinepolis',
+  standalone: false,
+  templateUrl: './cinepolis.component.html',
+  styleUrl: './cinepolis.component.css'
+})
+export class CinepolisComponent {
+
+  mensaje:string="";
+  nombre:string="";
+  TarjetaCine:boolean | undefined;
+  cantidadCompradores:number=0;
+  cantidadBoletos:number=0;
+  total:number=0;
+  totaldescuento:number=0;
+  totaldescuentotarjeta:number=0;
+  totalfinal:number=0;
+
+
+ calcular():void{
+  if(this.cantidadCompradores > 0){
+
+
+    if(this.cantidadBoletos <=  this.cantidadCompradores * 7 && this.cantidadBoletos != 0){
+      this.total = this.cantidadBoletos * 12;
+      if(this.cantidadBoletos > 5){
+        this.totaldescuento = this.total * 0.85;
+        this.totalfinal = this.totaldescuento;
+        this.mensaje = "Hola " + this.nombre + ", tu total es de $" + this.totalfinal;
+      }
+      if(this.cantidadBoletos >= 3 && this.cantidadBoletos <= 5){
+        this.totaldescuento = this.total * 0.9;
+        this.totalfinal = this.totaldescuento;
+        this.mensaje = "Hola " + this.nombre + ", tu total es de $" + this.totalfinal;
+      }
+      if(this.cantidadBoletos <= 2){
+        this.totaldescuento = this.total;
+        this.totalfinal = this.totaldescuento;
+        this.mensaje = "Hola " + this.nombre + ", tu total es de $" + this.totalfinal;
+      }
+      if(this.TarjetaCine === true){
+        this.totaldescuentotarjeta = this.totaldescuento * 0.9;
+        this.totalfinal = this.totaldescuentotarjeta;
+        this.mensaje = "Hola " + this.nombre + ", tu total es de $" + this.totalfinal;
+      }
+      else if(this.TarjetaCine === false){
+        this.totaldescuentotarjeta = this.totaldescuento;
+        this.totalfinal= this.totaldescuentotarjeta;
+        this.mensaje = "Hola " + this.nombre + ", tu total es de $" + this.totalfinal;
+      }
+    }
+    else{
+      this.mensaje = "Error, cantidad de boletos invalida";
+    }  
+  }else{
+    this.mensaje = "Error, cantidad de compradores invalida"
+  }
+  }
+}
---
+++ src/app/formularios/resistencias/operaciones-resistencias.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resistencias',
+  standalone: false,
+  templateUrl: './resistencias.component.html',
+  styleUrl: './resistencias.component.css'
+})
+export class ResistenciasComponent {
+
+  b1:number=0;
+  b2:number=0;
+  mult:number=0;
+  tol:number=0;
+
+
+}
